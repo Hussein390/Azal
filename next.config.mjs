662cbb03cf06fed9c540db3b2576625c8aebbd99
Your Name <hsssseeeen2002@gmail.com>
2025-02-19 16:48:08 +0300

    Change next.config from ts to mjs

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
+      },
+    ],
+  },
+  experimental: {
+    appDir: true, // If using the Next.js App Router
+  },
+};
+
+export default nextConfig;
