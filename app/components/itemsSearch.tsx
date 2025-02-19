b81d37d290a3137c0f1a0c9e743387338b1a696a
Your Name <hsssseeeen2002@gmail.com>
2025-02-19 16:37:20 +0300

    Add prisma to script

--- app/components/itemsSearch.tsx
+++ app/components/itemsSearch.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { createItemProps, getAnItem, getAPhone } from "../../backend/envirnoment";
+import { getAnItem, getAPhone } from "../../backend/envirnoment";
 import { DataPhones, ItemProps, PhoneProps } from './dataProvider';
 
 export function ItemsSearch() {
