9ebf721b10ec624180d821da0f9e1521bebd10f6
Your Name <hsssseeeen2002@gmail.com>
2025-02-20 09:23:32 +0300

    output: standalone added this line to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        pathname: '**',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
       },
     ],
   },
